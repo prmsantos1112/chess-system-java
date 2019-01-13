ae721eaa99d453566e36261bb7bb68c19a922baa
prmsantos1112 <prmsantosster@gmail.com>
2019-01-13 15:00:24 -0200

    Implementing possible moves of King : Chess

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -8,19 +9,74 @@ public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
-		
+
 	}
-	
+
 	@Override
 	public String toString() {
-		return "K";		
+		return "K";
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position pos = new Position(0, 0);
+
+		// Above - Acima;
+		pos.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Below - Para Baixo;
+		pos.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Left - Para Esquerda;
+		pos.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Right - Para Direita;
+		pos.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+		
+		// Nw - Noroeste;
+		pos.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Ne - Nordeste;
+		pos.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Sw - sudoeste;
+		pos.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
+		// Se - sudeste;
+		pos.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColumn()] = true;
+		}
+
 		return matriz;
 	}
-	
 
 }
